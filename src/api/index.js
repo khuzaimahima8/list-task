3681948b86d26dbfb31218f7df0edd8e8ed68a54
khuzaimahima8 <khuzaimahima8@gmail.com>
2025-06-21 06:02:31 +0700

    gitignore

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,5 @@
 export default {
-  async fetch(request, env, ctx) {
-    const url = new URL(request.url);
-
-    if(url.pathname.startsWith("/api/task")){
-      let {result} = await env.DB.prepare("SELECT * FROM task").all();
-      return Response.json(result);
-    }
-    return env.ASSETS.fetch(request);
-  },
-}
+  async fetch(request) {
+    return new Response("semangat!");
+  }
+};
