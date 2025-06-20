7a8286dd9a67ba97e0ad0a6d320cc5423b4cc63f
khuzaimahima8 <khuzaimahima8@gmail.com>
2025-06-20 09:52:51 +0700

    binding database

--- src/api/index.js
+++ src/api/index.js
@@ -3,33 +3,8 @@ export default {
     const url = new URL(request.url);
 
     if(url.pathname.startsWith("/api/task")){
-      return new Response.json([
-        {
-            id: '1',
-            name: "task 1",
-            description: "match",
-            deadline: 1741247092,
-      },
-      {
-            id: '2',
-            name: "task 2",
-            description: "english",
-            deadline: 1741247092,
-      },
-      {
-            id: '3',
-            name: "task 3",
-            description: "kimia",
-            deadline: 1741247092,
-      },
-      {
-             id: '4',
-            name: "task 4",
-            description: "biologi",
-            deadline: 1741247092,
-      },
-      
-        ]);  
+      let {result} = await env.DB.prepare("SELECT * FROM task").all();
+      return Response.json(result);
     }
     return env.ASSETS.fetch(request);
   },
