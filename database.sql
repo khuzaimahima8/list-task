72803236f0859b924f59aa239e7ca5eb18e79cbc
khuzaimahima8 <khuzaimahima8@gmail.com>
2025-06-24 22:57:34 +0700

    buat database

--- database.sql
+++ database.sql
@@ -1,7 +1,12 @@
-// buat tabel task
-create table task(
-    id INTEGER PRIMARY KEY
-    name TEXT NOT NULL,
-    description TEXT NOT NULL,
-    deadline DATE-TIME NOT NULL,
-)
+-- create table task(
+--     id INTEGER PRIMARY KEY,
+--     nama TEXT NOT NULL,
+--     keterangan TEXT NOT NULL,
+--     deadline DATE NOT NULL
+-- )
+
+-- sisipkan data awal
+-- INSERT INTO task (nama,keterangan,deadline) VALUES 
+-- ('tugas pemrograman', 'membuat CRUD', '2025-06-26');
+
+SELECT * FROM  task;
